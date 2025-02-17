bb2440bf16b083b2070f9241c22327f2c07ff46e
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-02-17 22:19:35 +0200

    fix: ffmpeg do in browser

--- components/CustomVideoPlayer.tsx
+++ components/CustomVideoPlayer.tsx
@@ -1,5 +1,9 @@
 import { useRef, useState, useEffect } from 'react';
 import { Play, Pause, Download, Music } from 'lucide-react';
+import { fetchFile, toBlobURL } from '@ffmpeg/util';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { Save } from 'lucide-react';
+import { set } from 'date-fns';
 
 export default function CustomVideoPlayer({ src, description }) {
   const videoRef = useRef(null);
@@ -8,6 +12,12 @@ export default function CustomVideoPlayer({ src, description }) {
   const [converting, setConverting] = useState(false);
   const [duration, setDuration] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
+
+  const [loaded, setLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const ffmpegRef = useRef(new FFmpeg());
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const messageRef = useRef<HTMLParagraphElement | null>(null);
 
   useEffect(() => {
     const video = videoRef.current;
@@ -41,33 +51,67 @@ export default function CustomVideoPlayer({ src, description }) {
     }
   };
 
-  const convertToMp3 = async () => {
+  const load = async () => {
+    setIsLoading(true);
+    const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.10/dist/umd';
+    const ffmpeg = ffmpegRef.current;
+
+    // Log messages from FFmpeg
+    ffmpeg.on('log', ({ message }) => {
+      if (messageRef.current) messageRef.current.innerHTML = message;
+    });
+
+    // Load FFmpeg WebAssembly files from the unpkg CDN
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+      wasmURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.wasm`,
+        'application/wasm'
+      ),
+    });
+
+    setLoaded(true);
+    setIsLoading(false);
+    console.log('FFmpeg is ready to use');
+  };
+
+  const convertToMP3 = async () => {
     setConverting(true);
-    try {
-      const response = await fetch('/api/facebook/audio', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ videoUrl: src }),
-      });
+    const ffmpeg = ffmpegRef.current;
 
-      if (!response.ok) {
-        throw new Error('Failed to convert video to audio');
-      }
+    // Download an example MP4 video file (replace this URL with your own video file URL)
+    await ffmpeg.writeFile('input.mp4', await fetchFile(src));
 
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = url;
-      a.download = `${description}.mp3`;
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error('Error converting to MP3:', error);
-    } finally {
-      setConverting(false);
-    }
+    console.log('Extract audio from the MP4 and convert to MP3');
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-vn',
+      '-ar',
+      '44100',
+      '-ac',
+      '2',
+      '-ab',
+      '192k',
+      'output.mp3',
+    ]);
+    console.log('Audio extracted and converted to MP3');
+    // Read the MP3 output file
+    const data = (await ffmpeg.readFile('output.mp3')) as any;
+    console.log('MP3 data:', data);
+    // download the blob data
+    const audioBlob = new Blob([data.buffer], { type: 'audio/mp3' });
+
+    // Create a URL for the audio blob to allow downloading
+    const audioUrl = URL.createObjectURL(audioBlob);
+    setMp3Url(audioUrl);
+
+    const link = document.createElement('a');
+    link.href = audioUrl;
+    link.download = `${description}.mp3`;
+    link.click();
+
+    setConverting(false);
   };
 
   const handleSeek = (e) => {
@@ -75,6 +119,10 @@ export default function CustomVideoPlayer({ src, description }) {
     videoRef.current.currentTime = newTime;
     setCurrentTime(newTime);
   };
+
+  useEffect(() => {
+    load();
+  }, []);
 
   return (
     <div className='relative w-full md:h-full h-full  lg:mx-auto lg:p-4 pb-4'>
@@ -128,7 +176,7 @@ export default function CustomVideoPlayer({ src, description }) {
           </a>
         ) : (
           <button
-            onClick={convertToMp3}
+            onClick={convertToMP3}
             className=' w-full text-center bg-red-600 mb-4 text-white py-2 rounded-lg shadow-lg hover:bg-red-700 flex justify-center items-center'
             disabled={converting}
           >
