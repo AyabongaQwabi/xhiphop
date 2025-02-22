6fe3104b3716cfa3beb388312ac7b1c152379c9c
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-02-22 03:29:40 +0200

    remove about

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,12 +33,6 @@ export function Navbar() {
             >
               Videos
             </Link>
-            <Link
-              href='/about'
-              className='text-gray-300 hover:text-yellow-400 transition duration-300 text-lg font-semibold'
-            >
-              About
-            </Link>
           </div>
         </div>
       </div>
